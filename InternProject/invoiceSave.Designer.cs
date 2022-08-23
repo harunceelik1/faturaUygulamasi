804e6a6d69d4a368171e2f6d7a91e2810498604e
harunceelik <Slaggo12@hotmail.com>
2022-08-23 12:51:39 +0300

    faturaUygulamasi

--- InternProject/invoiceSave.Designer.cs
+++ InternProject/invoiceSave.Designer.cs
@@ -371,6 +371,7 @@
             // 
             // groupBox1
             // 
+            this.groupBox1.Anchor = System.Windows.Forms.AnchorStyles.None;
             this.groupBox1.Controls.Add(this.label1);
             this.groupBox1.Controls.Add(this.txtTC);
             this.groupBox1.Controls.Add(this.label6);
